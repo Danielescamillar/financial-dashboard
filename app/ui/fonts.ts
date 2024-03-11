9e6672e3273074dbccb12c5d5faf4bddecc979c5
Danielescamillar <daniel.fernando.escamilla@gmail.com>
2024-03-11 14:09:57 -0500

    Lusitana font is added

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,13 @@
-import { Inter, Oswald } from 'next/font/google';
- 
+import { Inter, Oswald, Lusitana } from 'next/font/google';
+
 export const inter = Inter({ subsets: ['latin'] });
 
-export const oswald = Oswald({ 
+export const oswald = Oswald({
     weight: ['400', '700'],
-    subsets: ['latin'] });
+    subsets: ['latin']
+});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
