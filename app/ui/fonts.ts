1a6358a917c53718c576f5b7ae4b15894f3f42d4
Danielescamillar <daniel.fernando.escamilla@gmail.com>
2024-03-11 12:50:39 -0500

    base app is added

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Oswald } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const oswald = Oswald({ 
+    weight: ['400', '700'],
+    subsets: ['latin'] });
